d64e6cda06b57553bddc3637900f1faa979f9ba3
mariamkhan3300 <mariamkhan33000>
2024-10-02 15:10:11 +0500

    shift mongodb to online server

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require('express');
 const app = express();
 const db = require('./db');
 const bodyParser = require('body-parser');
+require('dotenv').config(); 
 
 app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
     res.send("Welcome to the hotel... How can I help?");
@@ -16,6 +19,8 @@ app.use('/menu', menuItemRoutes)
 const personRoutes = require('./routes/personRoutes');
 app.use('/person', personRoutes);
 
-app.listen(3000, () => {
+
+
+app.listen(PORT, () => {
     console.log('Hotel Server listening on port 3000!');
 })
